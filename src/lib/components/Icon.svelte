521df9b549608d7f93788a97415632d43256901f
Luca Bosin <bosluc01@students.zhaw.ch>
2023-11-12 15:48:51 +0100

    Add Icon component and @mdi/js dependency

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let d: string;
+  export let alt: string | undefined = undefined;
+</script>
+
+<svg class="icon" viewBox="0 0 24 24" aria-hidden={alt ? "true" : undefined} role={alt ? "img" : undefined}>
+  {#if alt}
+    <title>{alt}</title>
+  {/if}
+  <path {d} />
+</svg>
+
+<style>
+  .icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.125em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
