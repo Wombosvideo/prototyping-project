43da2e01216f0de23743f2735f838a71e7326e68
Luca Bosin <bosluc01@students.zhaw.ch>
2023-11-12 19:49:56 +0100

    Add Breadcrumb component with dynamic links

---
+++ src/lib/components/Breadcrumb.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+
+  export let links: { href: string; label: string }[] = [];
+
+  const genericLinks = [
+    { href: "/", label: "Home" },
+    { href: "/events", label: "Events" },
+    { href: "/events/new", label: "New Event" },
+    { href: "/venues", label: "Venues" },
+    { href: "/venues/new", label: "New Venue" },
+    { href: "/categories", label: "Categories" },
+    { href: "/categories/new", label: "New Category" },
+  ];
+
+  $: breadcrumbs = [...genericLinks, ...links].filter((link) => $page.url.pathname.startsWith(link.href));
+</script>
+
+<nav class="pb-4" aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    {#each breadcrumbs as { href, label }}
+      {@const active = $page.url.pathname === href}
+      <li class="breadcrumb-item" class:active aria-current={active ? "page" : undefined}>
+        {#if active}
+          {label}
+        {:else}
+          <a {href}>{label}</a>
+        {/if}
+      </li>
+    {/each}
+  </ol>
+</nav>
