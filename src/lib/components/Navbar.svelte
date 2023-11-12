3741262260c613de5d3abd25f67bcb9bb86c0d01
Luca Bosin <bosluc01@students.zhaw.ch>
2023-11-12 15:59:17 +0100

    Fix Navbar collapse issue

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -13,7 +13,7 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse">
+    <div id="navbarNav" class="collapse navbar-collapse">
       <ul class="navbar-nav">
         {#each [
           { href: '/', label: 'Home' },
@@ -21,7 +21,7 @@
           { href: '/venues', label: 'Venues' },
           { href: '/categories', label: 'Categories' }
         ] as { href, label}}
-          {@const active = (href === '/' && $page.url.pathname === '/') || $page.url.pathname.startsWith(href)}
+          {@const active = href === '/' ? $page.url.pathname === '/' : $page.url.pathname.startsWith(href)}
           <li class="nav-item">
             <a class="nav-link" href={$page.url.pathname === href ? "#" : href} class:active>{label}</a>
           </li>
