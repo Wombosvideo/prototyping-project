19c5acc7a4388df13a532fd4807ad84150c1dfc3
Luca Bosin <bosluc01@students.zhaw.ch>
2023-11-12 15:49:25 +0100

    Add Navbar component and update layout with dark mode support

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { mdiCalendarCheck } from "@mdi/js";
+	import Icon from "./Icon.svelte";
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container">
+    <a class="navbar-brand" href="/">
+      <Icon d={ mdiCalendarCheck } alt="Calendar" />
+      Event Manager
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        {#each [
+          { href: '/', label: 'Home' },
+          { href: '/events', label: 'Events' },
+          { href: '/venues', label: 'Venues' },
+          { href: '/categories', label: 'Categories' }
+        ] as { href, label}}
+          {@const active = (href === '/' && $page.url.pathname === '/') || $page.url.pathname.startsWith(href)}
+          <li class="nav-item">
+            <a class="nav-link" href={$page.url.pathname === href ? "#" : href} class:active>{label}</a>
+          </li>
+        {/each}
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<style>
+  .navbar-collapse {
+    flex-grow: 0;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
+	import Navbar from "$lib/components/Navbar.svelte";
+	import { onMount } from "svelte";
 
+  onMount(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.setAttribute('data-bs-theme', 'dark');
+    } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+      document.documentElement.setAttribute('data-bs-theme', 'light');
+    }
+  });
 </script>
 
-<slot />
+<Navbar />
+
+<main class="container my-5">
+  <slot />
+</main>
 
 <style>
-  
+  :root {
+    --bs-primary: #0E76A8;
+    --bs-primary-rgb: 14, 118, 168;
+  }
 </style>
