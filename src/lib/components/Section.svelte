c59bcaef7ec363f8bd91e3be46c3500ad281bad6
Luca Bosin <bosluc01@students.zhaw.ch>
2023-11-12 19:23:02 +0100

    Add Section component with title, body, and kind props.

---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let title: string;
+  export let body: string | undefined = undefined;
+  export let kind: "title" | "section" = "section";
+</script>
+
+<section class="mb-5">
+  <svelte:element this={kind === "title" ? "h1" : "h2"} class="mb-3">{title}</svelte:element>
+  {#if body}
+    <p>{body}</p>
+  {/if}
+  <slot />
+</section>
