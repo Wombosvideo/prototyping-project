b26e0fd1c00c1221a559c8706e39812eb77feb4d
Luca Bosin <bosluc01@students.zhaw.ch>
2023-12-22 21:07:29 +0100

    Add error on empty array

--- src/routes/api/venues/[venue]/+server.ts
+++ src/routes/api/venues/[venue]/+server.ts
@@ -1,6 +1,6 @@
-import { json } from "@sveltejs/kit";
-import type { RequestHandler } from "./$types";
 import { venues as collection } from "$lib/server/mongodb";
+import { error, json } from "@sveltejs/kit";
+import type { RequestHandler } from "./$types";
 
 export const GET: RequestHandler = async ({params}) => {
   const venues = (await collection.aggregate([
@@ -17,6 +17,10 @@ export const GET: RequestHandler = async ({params}) => {
     { '$set': { 'eventCount': { '$size': '$events' } } },
     { '$unset': 'events' }
   ]).toArray()).map(e => ({...e, _id: e._id.toString()})) as App.DTVenue[];
+
+  if (venues.length === 0)
+    throw error(404, "Venue not found");
+
   return json({
     status: "success",
     venue: venues[0]
