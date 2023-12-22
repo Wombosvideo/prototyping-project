4d2d2f07fde42e534a19383fccd9cb1ac28fadc5
Luca Bosin <bosluc01@students.zhaw.ch>
2023-12-22 17:49:10 +0100

    Add CategoryFilter component

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
 	import EventsList, { getUpcomingEvents, getPastEvents } from "$lib/components/EventsList.svelte";
-	import PageTitle from "$lib/components/PageTitle.svelte";
+	import PageTitle from "$lib/components/PageTitle.svelte";;
+	import CategoryFilter from "$lib/components/CategoryFilter.svelte";
+
+  let categories: string[] = [];
 </script>
 
 <PageTitle
   titleVisible="Events"
   description="A list of all events"
 />
+
+<CategoryFilter bind:selection={categories}/>
+
 <EventsList
   title="Upcoming Events"
   events={getUpcomingEvents()}
+  {categories}
 />
 <EventsList
   title="Past Events"
   events={getPastEvents()}
+  {categories}
 />
