def797aaa64aa0d1f14762a7e11eab9356e745bf
Luca Bosin <bosluc01@students.zhaw.ch>
2023-12-22 19:28:15 +0100

    Add list of all venues to Venues page

--- src/routes/venues/+page.svelte
+++ src/routes/venues/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	import PageTitle from "$lib/components/PageTitle.svelte";
+	import Section from "$lib/components/Section.svelte";
+
+  const getAllVenues = async () => {
+    const res = await fetch('/api/venues');
+    const json = await res.json();
+    const venues = json.venues as App.DTVenue[];
+    return venues;
+  };
 </script>
 
 <PageTitle
@@ -7,4 +15,29 @@
   description="A list of all event venues"
 />
 
-<!-- TODO: List of all venues -->
+<Section title="All Venues">
+  {#await getAllVenues()}
+    <p>Loading...</p>
+  {:then venues}
+    <div class="list-group">
+      {#each venues as { _id, name, city, country, photo: src, eventCount } (_id)}
+        <a href="/venues/{_id}" class="list-group-item list-group-item-action d-flex gap-3 p-3" aria-label="{name} - {city}: {eventCount} Events">
+          <img {src} alt={name} class="rounded-2 mr-3">
+          <div class="d-grid flex-grow-1">
+            <h5 class="mb-1">{name}</h5>
+            <small class="text-muted">{city}, {country}</small>
+          </div>
+          {#if eventCount > 0}
+            <span class="badge bg-primary align-self-start">{eventCount} Events</span>
+          {/if}
+        </a>
+      {/each}
+    </div>
+  {/await}
+</Section>
+
+<style>
+  img {
+    width: 6rem;
+  }
+</style>
