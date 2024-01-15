76a0bd50702b7fbe9a386c2366204ac5870d5906
Wombosvideo <bosluc01@students.zhaw.ch>
2024-01-15 09:23:46 +0100

    Refactor code to use API helpers

--- src/routes/api/users/[userId]/+server.ts
+++ src/routes/api/users/[userId]/+server.ts
@@ -1,18 +1,15 @@
 import { error, json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
-import { ObjectId } from "mongodb";
-import { users as collection } from "$lib/server/mongodb";
+import { getUsers } from "$lib/server/mongodb";
 
 export const GET: RequestHandler = async ({ params }) => {
-  const userDoc = await collection.findOne({ _id: new ObjectId(params.userId) });
+  const users = await getUsers(params.userId);
   
-  if (!userDoc)
+  if (users.length === 0)
     throw error(404, "User not found");
-
-  const user = {...userDoc, _id: userDoc._id.toString()} as App.DTUser;
 
   return json({
     status: "success",
-    user,
+    user: users[0],
   });
 };
